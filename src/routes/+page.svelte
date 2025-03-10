af315cea48fd8f13124e53a2b4451c3b78d92a4f
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2025-03-10 15:37:25 +0530

    feat: view PDF on fullscreen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import resume from '$lib/assets/resume.pdf';
+</script>
+
+<iframe
+    class="h-screen w-screen"
+    title="Peshala Prabhapoorna's Resume"
+    src={resume}>
+</iframe>
